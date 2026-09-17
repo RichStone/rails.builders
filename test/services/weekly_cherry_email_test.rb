cc77b8ae8195d1569724c8245e4de6ecdb899b69
Rich Steinmetz <hey@richsteinmetz.com>
2026-09-16 21:36:29 -0500

    Include the next Meet link in attendee emails

--- test/services/weekly_cherry_email_test.rb
+++ test/services/weekly_cherry_email_test.rb
@@ -82,6 +82,20 @@ class WeeklyCherryEmailTest < ActiveSupport::TestCase
     assert_includes render_for(@absentee).fetch(:text), "1st missed session in a row"
   end
 
+  test "attendees get the next session Meet link or its honest unavailable fallback" do
+    @next_session.update!(meet_url: "https://meet.google.com/abc-defg-hij")
+    email = render_for(@builder)
+    assert_includes email.fetch(:text), "Google Meet: https://meet.google.com/abc-defg-hij"
+    html = Nokogiri::HTML.fragment(email.fetch(:html))
+    assert_equal "https://meet.google.com/abc-defg-hij", html.at_css("a[href='https://meet.google.com/abc-defg-hij']").text
+
+    @next_session.update!(meet_url: nil)
+    email = render_for(@builder)
+    assert_includes email.fetch(:text), "The Google Meet link will be available on the session page."
+    assert_includes email.fetch(:html), "The Google Meet link will be available on the session page."
+    assert_not_includes email.fetch(:html), "https://meet.google.com/"
+  end
+
   test "absentee mentions are escaped and replace the no-mention sentence" do
     email = WeeklyCherryEmail.new(builder_session: @session, recipient: @absentee, subject: "Sharper offers", mentions: [ "Rich said: <b>your demo was useful</b>." ]).render
     assert_includes email.fetch(:text), "The Builders said this about you this time:"
